52eaeafd620ba24420260a862ae1b5b759c25b44
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2016-07-19 08:44:15 -0400

    Added Web Pages for Photos, Description, Contacting - Closing Issues 1-4
    -Routes to different each page by sending the html file
    -To Do:
    	-Create a Menu Bar at top of Screen that has options for which page to route to.
    	-Add that menu bar to all 4 pages

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ server.listen(port);
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/main.html');
 })
+
+app.get('/About', function(req, res){
+	res.sendFile(__dirname + '/aboutUs.html')
+})
+
+app.get('/Contact', function(req, res){
+	res.sendFile(__dirname + '/contact.html')
+})
+
+app.get('/Photos', function(req, res){
+	res.sendFile(__dirname + '/photos.html')
+})
